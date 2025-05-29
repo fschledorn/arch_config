0df0727d0220370adafe38c3a7f2ae5c06b36d56
fschledorn <nicolas.schledorn@stud.uni-heidelberg.de>
2025-05-29 15:14:45 +0200

    big refactor of the config giles

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+-- lua/plugins/colorscheme.lua
+return {
+  -- Maybe other themes like tokyonight or catppuccin here...
+
+  -- Add Oxocarbon
+  {
+    "shaunsingh/oxocarbon.nvim",
+    name = "oxocarbon",
+    priority = 1000,
+    opts = {},
+  },
+}

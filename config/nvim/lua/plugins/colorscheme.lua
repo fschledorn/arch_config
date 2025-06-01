17357138455cc74ca620ab555a1229d45fcce6ee
fschledorn <nicolas.schledorn@stud.uni-heidelberg.de>
2025-06-01 17:41:59 +0200

    [nvim] bump plugins

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -2,28 +2,11 @@
 return {
   -- Maybe other themes like tokyonight or catppuccin here...
 
-  -- Add nightfox
+  -- Add Oxocarbon
   {
-    "EdenEast/nightfox.nvim",
-    lazy = false,
+    "shaunsingh/oxocarbon.nvim",
+    name = "oxocarbon",
     priority = 1000,
-    opts = {
-      -- You can define a comment color for a specific theme, like carbonfox:
-      palettes = {
-        carbonfox = {
-          comment = "#cc5200", -- Replace with your desired hex color
-        },
-      },
-      options = {
-        terminal_colors = true,
-      },
-      styles = {
-        comments = "italic",
-      },
-    },
-    config = function(_, opts)
-      require("nightfox").setup(opts)
-      vim.cmd([[colorscheme carbonfox]])
-    end,
+    opts = {},
   },
 }
